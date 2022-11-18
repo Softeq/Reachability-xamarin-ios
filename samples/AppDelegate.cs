e9d2ba52d525ec7db3f4e3e58775a1ff1eb59913
Yauheni Pakala <evgeniy.pakalo@gmail.com>
2022-11-18 13:47:35 +0200

    Add sample

--- samples/AppDelegate.cs
+++ samples/AppDelegate.cs
@@ -1,30 +1,66 @@
+using System.Diagnostics;
+using ObjCRuntime;
+using ReachabilityBindings;
+
 namespace ReachabilitySamples;
 
-[Register ("AppDelegate")]
-public class AppDelegate : UIApplicationDelegate {
-	public override UIWindow? Window {
-		get;
-		set;
-	}
+[Register("AppDelegate")]
+public class AppDelegate : UIApplicationDelegate
+{
+    private Reachability? _reach;
 
-	public override bool FinishedLaunching (UIApplication application, NSDictionary launchOptions)
-	{
-		// create a new window instance based on the screen size
-		Window = new UIWindow (UIScreen.MainScreen.Bounds);
+    public override UIWindow? Window { get; set; }
 
-		// create a UIViewController with a single UILabel
-		var vc = new UIViewController ();
-		vc.View!.AddSubview (new UILabel (Window!.Frame) {
-			BackgroundColor = UIColor.SystemBackground,
-			TextAlignment = UITextAlignment.Center,
-			Text = "Hello, iOS!",
-			AutoresizingMask = UIViewAutoresizing.All,
-		});
-		Window.RootViewController = vc;
+    public override bool FinishedLaunching(UIApplication application, NSDictionary launchOptions)
+    {
+        // create a new window instance based on the screen size
+        Window = new UIWindow(UIScreen.MainScreen.Bounds);
 
-		// make the window visible
-		Window.MakeKeyAndVisible ();
+        // create a UIViewController with a single UILabel
+        var vc = new UIViewController();
+        vc.View!.AddSubview(new UILabel(Window!.Frame)
+        {
+            BackgroundColor = UIColor.SystemBackground,
+            TextAlignment = UITextAlignment.Center,
+            Text = "Hello, iOS!",
+            AutoresizingMask = UIViewAutoresizing.All,
+        });
+        Window.RootViewController = vc;
 
-		return true;
-	}
+        // make the window visible
+        Window.MakeKeyAndVisible();
+
+
+
+        // Allocate a reachability object
+        _reach = Reachability.ReachabilityForInternetConnection;// ReachabilityWithHostName("www.google.com");
+
+
+        //var handle = Messaging.IntPtr_objc_msgSend(classHandle, selector);
+        //var a = Runtime.GetNSObject<Reachability>(classHandle);
+
+        // Set the blocks
+        _reach.ReachableBlock = (Reachability r) =>
+            {
+                // keep in mind this is called on a background thread
+                // and if you are updating the UI it needs to happen
+                // on the main thread, like this:
+
+                BeginInvokeOnMainThread(() =>
+                {
+                    Debug.WriteLine("REACHABLE");
+                    Debug.WriteLine(r.CurrentReachabilityString);
+                });
+            };
+
+
+        _reach.UnreachableBlock = (Reachability r) =>
+                {
+                    Debug.WriteLine("UNREACHABLE");
+                };
+
+        _reach.StartNotifier();
+
+        return true;
+    }
 }
